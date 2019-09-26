353940e6fc14cb79d1187fe2c1eca92266f65c4f
varun mara <varungowde@gmail.com>
2019-09-26 00:39:07 -0500

    edited files

---
+++ src/components/introduction.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>
+                            Hi! <br />
+                            I'm Meg LOL!
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href=""
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Checkme out my Ass LOL!
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>
+                            I love everything
+                            <br /> LOL!
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href=""
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Checkout my butt LOL!{" "}
+                              <i className="icon-briefcase3" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>
+                            I often <br />
+                            go beresk ...{" "}
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href=""
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              I'm MAD LOL! <i className="icon-book" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
